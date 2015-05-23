e675802a0fc85101eb0ef6655409ca9b28c2bffc
Kendric Jones <kendricjones@BradMacPro.local>
2015-05-23 11:26:49 -0400

    Authorization on links

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150523143608) do
+ActiveRecord::Schema.define(version: 20150523145659) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20150523143608) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.string   "index"
   end
 
   create_table "users", force: :cascade do |t|
