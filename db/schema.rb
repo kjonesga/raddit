0ed636b2e6e24841e6ce641e9cc5cbfb84b13364
Kendric Jones <kendricjones@BradMacPro.local>
2015-05-23 10:41:02 -0400

    Add association between Link and User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522225250) do
+ActiveRecord::Schema.define(version: 20150523143608) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
