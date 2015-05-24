0497c860690687f043be731d8f63dbca6afe211d
Kendric Jones <kendricjones@BradMacPro.local>
2015-05-24 18:10:24 -0400

    Added Index to User

--- db/migrate/20150523145659_add_user_id_to_links.rb
+++ db/migrate/20150523145659_add_user_id_to_links.rb
@@ -1,6 +1,6 @@
 class AddUserIdToLinks < ActiveRecord::Migration
   def change
     add_column :links, :user_id, :integer
-    add_column :links, :index, :string
+    add_index :links, :user_id
   end
 end
